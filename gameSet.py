51194252fa71966e9b1486d004b65f29bd893316
jacobjauregui <jacob_jauregui_23outlook.com>
2022-12-19 05:56:36 -0600

    Buttons to make board and start initail position

--- gameSet.py
+++ gameSet.py
@@ -1,104 +1,88 @@
-class Piece:
-    def __init__(self, piece_id, piece_name, color_team, max_range, r_value):
-        self.piece_id = piece_id
-        self.piece_name = piece_name
-        self.color_team = color_team
-        self.max_range = max_range
-        self.r_value = r_value
-        self.moved = False
-        self.attacked = False
-        self.captured = False
-        self.selected = False
+moved = False
+attacked = False
+captured = False
+selected = False
+moves = {}
+row = []
+board = []
 
-    def selectPiece(self):
-        self.col = input('select Col')
-        self.row = input('select Row')
+def make_rows():
+    column = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    piece = "--"
+    board = []
+    row = []
+    for i, col in enumerate(column):
+        for j in range(8):
+            square = [f'{col}{(8-j)}', piece]
+            board.append(square)        
+            row = [board[r::8] for r in range(8)]
+    return row
 
-        print(self.selected)
+def start_position():
+    row = make_rows()
+    for bp in row[1]:
+        bp[1] =  "bP"
+    for wp in row[6]:
+        wp[1] = "wP"
+    for br in row[0][::7]:
+        br[1] = "bR"
+    for wr in row[7][::7]:
+        wr[1] = "wR"
+    for bn in row[0][1::5]:
+        bn[1] = "bN"
+    for wn in row[7][1::5]:
+        wn[1] = "wN"
+    for bb in row[0][2::3]:
+        bb[1] = "bB"
+    for wb in row[7][2::3]:
+        wb[1] = "wB"
+    row[0][3][1] = "bQ"
+    row[0][4][1] = "bK"
+    row[7][3][1] = "wQ"
+    row[7][4][1] = "wK"
+    initial_pos = row
+    return initial_pos
 
-        return self.selected
+def print_pos():
+    position = start_position()
+    print(position)
 
-    def move(self):
-        final_col = input(int('select destiny col'))
-        final_row = input(int('select destiny row'))
-    
-    def capture(self):
-        pass
 
-x_ghost =  Piece("--", "Ghost", None, 0, 0)
-b_pawn =   Piece("bP", "Pawn", "BLACK", 1, 1)
-w_pawn =   Piece("wP", "Pawn", "WHITE", 1, 1) 
-b_rook =   Piece('bR', "Rook", "BLACK", 7, 5)
-w_rook =   Piece("wR", "Rook", "WHITE", 7, 5)
-b_knight = Piece("bN", "Knight", "BLACK", 2, 3)
-w_knight = Piece("wN", "Knight", "WHITE", 2, 3)
-b_bishop = Piece("bB", "Bishop", "BLACK", 7, 3)
-w_bishop = Piece("wB", "Bishop", "WHITE", 7, 3)
-b_queen =  Piece("bQ", "Queen", "BLACK", 7, 9)
-w_queen =  Piece("wQ", "Queen", "WHITE", 7, 9)
-b_king =   Piece("bK", "King", "BLACK", 1, 100)
-w_king =   Piece("wK", "King", "WHITE", 1, 100)
+'''
 
-board = [
-    [["a", 8, "--"], ["b", 8, "--"], ["c", 8, "--"], ["d", 8, "--"], ["e", 8, "--"], ["f", 8, "--"], ["g", 8, "--"], ["h", 8, "--"]],
-    [["a", 7, "--"], ["b", 7, "--"], ["c", 7, "--"], ["d", 7, "--"], ["e", 7, "--"], ["f", 7, "--"], ["g", 7, "--"], ["h", 7, "--"]],
-    [["a", 6, "--"], ["b", 6, "--"], ["c", 6, "--"], ["d", 6, "--"], ["e", 6, "--"], ["f", 6, "--"], ["g", 6, "--"], ["h", 6, "--"]],
-    [["a", 5, "--"], ["b", 5, "--"], ["c", 5, "--"], ["d", 5, "--"], ["e", 5, "--"], ["f", 5, "--"], ["g", 5, "--"], ["h", 5, "--"]],
-    [["a", 4, "--"], ["b", 4, "--"], ["c", 4, "--"], ["d", 4, "--"], ["e", 4, "--"], ["f", 4, "--"], ["g", 4, "--"], ["h", 4, "--"]],
-    [["a", 3, "--"], ["b", 3, "--"], ["c", 3, "--"], ["d", 3, "--"], ["e", 3, "--"], ["f", 3, "--"], ["g", 3, "--"], ["h", 3, "--"]],
-    [["a", 2, "--"], ["b", 2, "--"], ["c", 2, "--"], ["d", 2, "--"], ["e", 2, "--"], ["f", 2, "--"], ["g", 2, "--"], ["h", 2, "--"]],
-    [["a", 1, "--"], ["b", 1, "--"], ["c", 1, "--"], ["d", 1, "--"], ["e", 1, "--"], ["f", 1, "--"], ["g", 1, "--"], ["h", 1, "--"]],
-]
 
-# initial position of all pieces
+def select_square(self):
+    self.Piece.selected = False
+    for r, rows in enumerate(row.start_position()):
+        for s in self.rows:
+            if self.square in s:
+                ini = f"{s[0]}"
+                piece = f"{s[1]}"
+                selected = True
+                return ini
+def locate(self):
+    if "nN" in self.row:
+        print("nN")
 
-board[0][0][2] = b_rook.piece_id
-board[0][7][2] = b_rook.piece_id
-board[0][1][2] = b_knight.piece_id
-board[0][6][2] = b_knight.piece_id
-board[0][2][2] = b_bishop.piece_id
-board[0][5][2] = b_bishop.piece_id
-board[0][3][2] = b_queen.piece_id
-board[0][4][2] = b_king.piece_id
-board[1][0][2] = b_pawn.piece_id
-board[1][1][2] = b_pawn.piece_id
-board[1][2][2] = b_pawn.piece_id
-board[1][3][2] = b_pawn.piece_id
-board[1][4][2] = b_pawn.piece_id
-board[1][5][2] = b_pawn.piece_id
-board[1][6][2] = b_pawn.piece_id
-board[1][7][2] = b_pawn.piece_id
-
-board[7][0][2] = w_rook.piece_id
-board[7][7][2] = w_rook.piece_id
-board[7][1][2] = w_knight.piece_id
-board[7][6][2] = w_knight.piece_id
-board[7][2][2] = w_bishop.piece_id
-board[7][5][2] = w_bishop.piece_id
-board[7][3][2] = w_queen.piece_id
-board[7][4][2] = w_king.piece_id
-board[6][0][2] = w_pawn.piece_id
-board[6][1][2] = w_pawn.piece_id
-board[6][2][2] = w_pawn.piece_id
-board[6][3][2] = w_pawn.piece_id
-board[6][4][2] = w_pawn.piece_id
-board[6][5][2] = w_pawn.piece_id
-board[6][6][2] = w_pawn.piece_id
-board[6][7][2] = w_pawn.piece_id
-
-# Some moves to test the board
-
-board[6][4][2] = x_ghost.piece_id
-board[4][4][2] = w_pawn.piece_id
-
-board[1][4][2] = x_ghost.piece_id
-board[3][4][2] = b_pawn.piece_id
-
-board[7][6][2] = x_ghost.piece_id
-board[5][5][2] = w_knight.piece_id
-
-board[0][1][2] = x_ghost.piece_id
-board[2][2][2] = b_knight.piece_id
-
-board[7][5][2] = x_ghost.piece_id
-board[4][2][2] = w_bishop.piece_id
+print(self.row[0])
+#def move_piece(ini, end):
+#    row = start_position()
+#    if  piece != "--":
+#        moves = {
+#            ini.square_selected("e4"):end 
+#        }
+#        row = moves
+#        print(piece)
+#        print(ini)
+#        print(end)
+#        #if piece in ini:
+#
+#        #for r, rows in enumerate(row):
+#        #    for move in rows:
+#        #        for piece in move:
+#        #print(piece)
+#            #print(moves)
+#            #print(row)
+#
+#move_piece( "e4") 
+'''
--- main.py
+++ main.py
@@ -1,60 +1,87 @@
 import tkinter as tk
-import gameSet as sets
-class Interface:
+import gameSet as gs 
+class JakeChess:
     def __init__(self, size):
         self.size = size 
-        self.bd = sets.board
         self.assets = {}
         self.window = tk.Tk()
         self.window.title("JakeChess")
         self.window.iconbitmap("./assets/chessicon.ico")
-        self.window.geometry(f"{(self.size+1)*8}x{(self.size+1)*8}")
+        self.window.geometry("800x640")
         self.window.resizable(True, True)
         self.interface = tk.Canvas(self.window, borderwidth=0, cursor="hand2")
         self.interface.pack(fill="both", expand=True)
-        
+        self.board_drawing = False
+        self.row = gs.make_rows()
+        self.initial_pos = gs.start_position()
+
     def __call__(self):
         self.window.mainloop()
-    
-    def drawBoard(self):
+
+    # BOARD
+    def draw_board(self):
         for i in range(8):
             for j in range(8):
                 if (i+j) % 2 == 0:
-                    self.interface.create_rectangle(i*self.size, j*self.size, (i+1)*self.size, (j+1)*self.size, fill="#f0d9b5")
+                    self.interface.create_rectangle(
+                        i*self.size,
+                        j*self.size,
+                        (i+1)*self.size,
+                        (j+1)*self.size,
+                        fill="#f0d9b5"
+                    )
                 else:
-                    self.interface.create_rectangle(i*self.size, j*self.size, (i+1)*self.size, (j+1)*self.size, fill="#b58863")
-
-    def squareName(self):
+                    self.interface.create_rectangle(
+                        i*self.size,
+                        j*self.size,
+                        (i+1)*self.size,
+                        (j+1)*self.size,
+                        fill="#b58863"
+                    )
         for i in range(8):
             for j in range(8):
                 self.interface.create_text(
                     (i+0.88)*self.size,
                     (j+0.88)*self.size,
-                    text=(f"{self.bd[j][i][0]}{self.bd[j][i][1]}"),
+                    text=(f"{self.row[j][i][0]}"),
                     anchor="center",
                 )
-
-    def loadAssets(self):
+        self.board_drawing = True
+        self.btn_startGame()
+# GAME    
+    def new_game(self):
         pieces = ["bB", "bK", "bN", "bP", "bQ", "bR", "wB", "wK", "wN", "wP", "wQ", "wR"]
         for piece in pieces:
-            self.assets[piece] = tk.PhotoImage(file ="./assets/" + piece + ".png")
-
-    def showPieces(self):
-        for x in range(8):
-            for y in range(8):
-                if self.bd[y][x][2] != "--":
-                    self.interface.create_image(
-                        (x+0.5)*self.size, 
-                        (y+0.5)*self.size,
-                        image=self.assets[self.bd[y][x][2]],
+            self.assets[piece] = tk.PhotoImage(file="./assets/" + piece + ".png")
+        if self.board_drawing == True:
+            gs.start_position()
+            for i in range(8):
+                for j in range(8):
+                    if self.initial_pos[j][i][1] != "--":
+                        self.interface.create_image(
+                        (i+0.5)*self.size, 
+                        (j+0.5)*self.size,
+                        image=self.assets[self.initial_pos[j][i][1]],
                         anchor="center"
                     )
-                    
-JakeChess = Interface(70)
+        else:
+            pass
 
-JakeChess.drawBoard()
-JakeChess.squareName()
-JakeChess.loadAssets()
-JakeChess.showPieces()
+    def btn_drawBoard(self):
+        tk.Button(
+            self.window,
+            text="Draw Board",
+            command=self.draw_board 
+        ).pack()
 
-JakeChess()
+    def btn_startGame(self):
+        tk.Button(
+            self.window,
+            text="New Game",
+            command=self.new_game
+        ).pack()
+
+Menu = JakeChess(70)
+Menu.btn_drawBoard()
+
+Menu()
